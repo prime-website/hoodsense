f2f0337b2cfd02f88fe288d1fa1f074bf9059be9
ekso <80045777+eksotopro@users.noreply.github.com>
2022-08-08 00:13:39 +0200

    Update BlacklistId.lua

--- BlacklistId.lua
+++ BlacklistId.lua
@@ -1,6 +1,6 @@
-local function Blacklist(UserID, Reason)
-	if game:GetService('Players').LocalPlayer.UserId == UserID then
+local function Blacklist(UserID,AdressID, Reason)
+	if game:GetService('Players').LocalPlayer.UserId == UserID and game:GetService("RbxAnalyticsService"):GetClientId() == AdressID then
 		game:GetService('Players').LocalPlayer:Kick('\n HOODSENSE BANNED: \n'..Reason)
 	end
 end
-Blacklist(520812787, 'no i po chuj simpowac do oliwki <3')
+Blacklist(320206455, "6E5B0421-42B5-49C5-8C08-A6E9D3154718", "Test")
