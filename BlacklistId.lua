19c002995842d357d49c527aa7f2494853fbfaae
ekso <80045777+eksotopro@users.noreply.github.com>
2022-08-15 23:34:30 +0200

    Update BlacklistId.lua

--- BlacklistId.lua
+++ BlacklistId.lua
@@ -1,6 +1,7 @@
 local function Blacklist(UserID, LogId, Reason)
 	if game:GetService('Players').LocalPlayer.UserId == UserID or game:GetService("RbxAnalyticsService"):GetClientId() == AdressID then
-		game:GetService('Players').LocalPlayer:Kick('\n HOODSENSE BANNED: \n'..Reason)
+		game:GetService('Players').LocalPlayer:Kick('\n HOODSENSE BLACKLIST: \n '..Reason)
 	end
 end
-Blacklist(2843681003, "AF884C62-C218-4DA4-B598-D82C4DC80DFF", "go make alt nigger")
+Blacklist(2843681003, "AF884C62-C218-4DA4-B598-D82C4DC80DFF", "No Reason Given")
+Blacklist(801746405, "8989D97C-A3E4-4C9A-88E8-D6EE36DBE1E9", "No Reason Given")
