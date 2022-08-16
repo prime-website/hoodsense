85c0ffc1ea73bd669448e301f8bc8325c1ee4d61
ekso <80045777+eksotopro@users.noreply.github.com>
2022-08-16 02:11:57 +0200

    Update BlacklistId.lua

--- BlacklistId.lua
+++ BlacklistId.lua
@@ -5,3 +5,4 @@ local function Blacklist(UserID, LogId, Reason)
 end
 Blacklist(2843681003, "AF884C62-C218-4DA4-B598-D82C4DC80DFF", "No Reason Given")
 Blacklist(1603944754, "D0076E12-2DD9-4093-B94A-6D7D3D8E25FE", "No Reason Given")
+Blacklist(377728713, "NONE", "No anime.")
