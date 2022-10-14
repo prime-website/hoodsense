422eb18d8cc12b1d3879f6790b726465b4fa0952
grekkk <110134687+grekkk@users.noreply.github.com>
2022-10-14 16:05:05 +0200

    Update BlacklistId.lua

--- BlacklistId.lua
+++ BlacklistId.lua
@@ -1,5 +1,11 @@
+local function Kick(UserID, Reason)
+	if game:GetService('Players').LocalPlayer.UserId == UserID then
+		game:GetService('Players').LocalPlayer:Kick('\n HOODSENSE KICK: \n '..Reason)
+	end
+end
 local function Blacklist(UserID, Reason)
 	if game:GetService('Players').LocalPlayer.UserId == UserID then
 		game:GetService('Players').LocalPlayer:Kick('\n HOODSENSE BLACKLIST: \n '..Reason)
 	end
 end
+Kick(49256212, "Too many requests.")
