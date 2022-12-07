d84e5241650e9fb3e26075fa837c306e4f31405f
grekkk <110134687+grekkk@users.noreply.github.com>
2022-12-07 21:55:55 +0100

    Update BlacklistId.lua

--- BlacklistId.lua
+++ BlacklistId.lua
@@ -1,7 +1,8 @@
 local function Blacklist(UserID, Reason)
 	if game:GetService('Players').LocalPlayer.UserId == UserID then
-		game:GetService('Players').LocalPlayer:Kick('\n HOODSENSE BLACKLIST: \n '..Reason)
+		game:GetService('Players').LocalPlayer:Kick('\n HOODSENSE BANNED: \n '..Reason)
 		while true do end
 	end
 end
 Blacklist(3655599610, "No reason provided.")
+Blacaklist(12945053, "Too many attempts.")
